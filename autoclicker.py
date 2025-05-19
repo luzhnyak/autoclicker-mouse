946a19e59cd14801e323698ac6eb5619ce654c38
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2025-05-19 19:32:17 +0300

    update interval

--- autoclicker.py
+++ autoclicker.py
@@ -3,7 +3,7 @@ import time
 import keyboard
 
 
-def autoclicker(interval=5, toggle_key='f'):
+def autoclicker(interval=3, toggle_key="f"):
     print(f"Натисніть '{toggle_key}' для запуску/зупинки...")
     running = False
     base_x, base_y = pyautogui.position()
@@ -15,7 +15,7 @@ def autoclicker(interval=5, toggle_key='f'):
             base_x, base_y = pyautogui.position()
             running = not running
             print("Автоклікер запущено" if running else "Автоклікер зупинено")
-            
+
             # Запобігання багаторазовому спрацюванню на одне натискання
             time.sleep(0.5)
 
